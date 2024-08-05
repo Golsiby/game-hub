130619cb2c5dcb1b4b1288c3d7868290fe2b77be
golsiby <golsiby@yahoo.com>
2024-08-05 23:28:08 +0330

    Get optimized images

---
+++ src/Services/Image-URL.ts
@@ -0,0 +1,7 @@
+const getCroppetImageUrl = (url: string) => {
+    const token = 'media/';
+    const index = url.indexOf(token) + token.length;
+    return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+}
+
+export default getCroppetImageUrl;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../Hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppetImageUrl from "../Services/Image-URL";
 
 interface Props {
   game: Game;
@@ -10,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow={"hidden"}>
-      <Image src={game.background_image}></Image>
+      <Image src={getCroppetImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justify="space-between">
